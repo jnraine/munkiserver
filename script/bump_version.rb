8d391567d806f8a15cf77da7d71aa0b65df20f20
Jordan Raine <jraine@sfu.ca>
2010-06-17 15:28:28 -0700

    - Version 0.5.3.0 - Added script/bump_version.rb to simplify constant version bumping

---
+++ script/bump_version.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# Must be called from Rails.root
+# Open VERSION file
+path = `pwd`.chomp + "/config/VERSION"
+version = File.read(path)
+a = version.split(".")
+a[a.length - 1] = a[a.length - 1].to_i + 1
+version = a.join(".")
+File.open(path, 'w') {|f| f.write(version) }
