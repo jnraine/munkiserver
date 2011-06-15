f82815338e7d0f1a86a7357bcd26c1ae1f29fc03
Jordan Raine <jnraine@gmail.com>
2011-06-14 22:16:05 -0700

    - Bug fix for non UTF-8 ManagedInstallReport plists uploaded to computer#checkin

--- config/initializers/extend_string.rb
+++ config/initializers/extend_string.rb
@@ -6,4 +6,18 @@ class String
   def from_plist
     Plist.parse_xml(self)
   end
+  
+  # Convert string from whatever encoding to UTF-8, covering corner cases
+  def to_utf8
+    begin
+      self.encode("UTF-8")
+    rescue Encoding::UndefinedConversionError => e
+      forced = self.force_encoding("UTF-8")
+      if forced.valid_encoding?
+        forced
+      else
+        raise EncodingError("Unable to convert string from #{self.encoding} to UTF-8. The encode method threw " + e + " and forced encoding failed to produce a valid string encoding")
+      end
+    end
+  end
 end
