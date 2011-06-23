ca4485b12cd40313e38549e536ee6fb441c58eb8
Jonathan <hujonathan89@gmail.com>
2011-06-23 15:42:30 -0700

    rollback to previous commit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -225,16 +225,8 @@ $(document).ready(function() {
 	        downloadTable: $('.download_files'),
 	        buildUploadRow: function (files, index) {
 	            var file = files[index];
-	            return $('<tr><td>' + file.name + '<\/td>' +
-	                    '<td class="file_upload_progress"><div><\/div><\/td>' +
-	                    '<td class="file_upload_cancel">' +
-	                    '<button class="ui-state-default ui-corner-all" title="Cancel">' +
-	                    '<span class="ui-icon ui-icon-cancel">Cancel<\/span>' +
-	                    '<\/button><\/td><\/tr>');
-	        },
-	        buildDownloadRow: function (file) {
-	            return $('<tr><td><img alt="Photo" width="40" height="40" src="' + file.pic_path + '">' + file.name + '<\/td><\/tr>');
-	        },
+	            return $('<tr><td>' + file.name + '<\/td>');
+	        }
 	    });
 }); // end document ready function
 
