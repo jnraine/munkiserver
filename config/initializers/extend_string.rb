e682bef8c1ea834493ce94ef11518f8b2b8655e1
Jordan Raine <jnraine@gmail.com>
2011-06-23 18:37:10 -0700

    - Improved version string comparison

--- config/initializers/extend_string.rb
+++ config/initializers/extend_string.rb
@@ -20,4 +20,41 @@ class String
       end
     end
   end
+
+  # Compare a version string (e.g., "7.0.1.2") with self.
+  # Returns 1, 0, -1: greater than, equal, less than, respectively.  Similar to <=>.
+  def version_string_comparison(string)
+    unless string.instance_of?(String)
+      raise ArgumentError.new("Must pass string for version string comparison. Got a #{string.class} instance.")
+    end
+
+    # Convert to array of ints (if possible), snipping off any guff
+    f_split = self.gsub(/(\.0)+$/,'').split(".").map { |e| e.match(/^\d+$/) ? e.to_i : e }
+    s_split = string.gsub(/(\.0)+$/,'').split(".").map { |e| e.match(/^\d+$/) ? e.to_i : e }
+    
+    # Compare the array elements
+    i = 0
+    comparison_results = []
+    while(i < f_split.length or i < s_split.length) do
+      if f_split[i].nil? or s_split[i].nil?
+        # One results is nil
+        comparison_results << 1 if f_split[i]
+        comparison_results << -1 if s_split[i]
+      elsif f_split[i].instance_of?(String) or s_split[i].instance_of?(String)
+        # Compare as strings
+        comparison_results << f_split[i].to_s.<=>(s_split[i].to_s)
+      else
+        # Compare as integers
+        comparison_results << f_split[i].<=>(s_split[i])
+      end
+      i += 1
+    end
+    
+    # Return comparison result, if strings not equal
+    comparison_results.each do |comparison_result|
+      return comparison_result unless comparison_result == 0
+    end
+    # If we made it here, they are equal
+    return 0
+  end
 end
