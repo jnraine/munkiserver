2ac7bfc300d7c549e0a9b6a7c4ddc28613e04334
Jonathan <hujonathan89@gmail.com>
2011-07-07 15:38:10 -0700

    -bug fix, point the correct ClientIdentifier value

--- app/models/manifest/computer.rb
+++ app/models/manifest/computer.rb
@@ -119,7 +119,8 @@ class Computer < ActiveRecord::Base
   end
 
   def client_identifier
-    self.class.to_s.tableize + "/" + mac_address + ".plist"
+    # self.class.to_s.tableize + "/" + mac_address + ".plist"
+    mac_address + ".plist"
   end
   
   # Validates the presence of a computer model
