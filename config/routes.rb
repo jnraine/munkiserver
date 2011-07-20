a3e52e5fd002006d029d0e6c0c8f938ee96ee53a
Phil Monroe <phil@philmonroe.com>
2011-07-20 10:45:29 -0400

    Added route to enable OU style checkins

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Munki::Application.routes.draw do
 
   # Make munki-client-friendly URLs
   match ':id.plist', :controller => 'computers', :action => 'show', :format => 'manifest', :id => /[A-Za-z0-9_\-\.%:]+/
+  match 'computers/:id.plist', :controller => 'computers', :action => 'show', :format => 'manifest', :id => /[A-Za-z0-9_\-\.%:]+/
+  
   match 'catalogs/:unit_id-:environment_name.plist' => 'catalogs#show', :format => 'plist'
   match 'pkgs/:id' => 'packages#download', :as => 'download_package', :id => /[A-Za-z0-9_\-\.%]+/
   match '/configuration/:id.plist', :controller => 'computers', :action => 'show', :format => 'client_prefs', :id => /[A-Za-z0-9_\-\.:]+/
