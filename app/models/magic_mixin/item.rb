c0b63fb8ff8b14f62ec41692917772ad10f127f6
Jordan Raine <jnraine@gmail.com>
2011-07-21 16:01:29 -0700

    Modified Item magic mixin and package relationships

--- app/models/magic_mixin/item.rb
+++ app/models/magic_mixin/item.rb
@@ -9,16 +9,17 @@ module Item
       # = Code start here! =
       # ====================
       
-      belongs_to :manifest, :polymorphic => true
+      belongs_to :manifest, :polymorphic => true # package, computer, computer group, bundle
       belongs_to :package_branch
+      belongs_to :package
       
       # Returns the appropriate package for this package branch
       # relationship.  It does this by first checking if a package
       # ID is specified, grabbing a package if it exists.  If that fails
       # it grabs the latest package from the package branch and returns that
       def package
-        p = Package.find(package_id) if package_id?
-        p ||= package_branch.latest(manifest)
+        p = super
+        p ||= package_branch.latest(manifest) if package_branch.present?
         p
       end
       
