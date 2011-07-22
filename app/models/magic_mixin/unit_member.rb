d20a78182b4ea0c949954a96f3a303843f7aef14
Jordan Raine <jnraine@gmail.com>
2011-07-22 10:54:40 -0700

    Improved unit_member unit scope and removed computer_group bootstrap task

--- app/models/magic_mixin/unit_member.rb
+++ app/models/magic_mixin/unit_member.rb
@@ -14,7 +14,7 @@ module UnitMember
       belongs_to :unit
       belongs_to :environment
       
-      scope :unit, lambda { |u| where(:unit_id => u.id) }
+      scope :unit, lambda { |u| u.present? ? where(:unit_id => u.id) : where(:unit_id => nil) }
       scope :environment, lambda { |p| where(:environment_id => p.id) }
       scope :environment_ids, lambda { |ids| where(:environment_id => ids) }
       scope :environments, lambda { |p| where(:environment_id => p.collect(&:id)) }
