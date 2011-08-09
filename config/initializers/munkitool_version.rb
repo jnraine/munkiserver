fe7546c9ec6ca32daa1209ce810a4bebd78cad0f
Phil Monroe <phil@philmonroe.com>
2011-08-09 14:12:49 -0400

    made the munkitools version initializer a little more futureproof for munkitools updates

--- config/initializers/munkitool_version.rb
+++ config/initializers/munkitool_version.rb
@@ -1,12 +1,5 @@
 if Munki::Application::MUNKI_TOOLS_AVAILABLE
-  version_path = Pathname.new("/usr/local/munki/munkilib/version.plist")
-  svn_version_path = Pathname.new("/usr/local/munki/munkilib/svnversion")
-  
-  munki_version = Plist::parse_xml(version_path)["CFBundleShortVersionString"]
-  
-  munki_build = File.new(svn_version_path, "r").gets.strip
-  
-  Munki::Application::MUNKITOOLS_VERSION = "#{munki_version} (#{munki_build})"
+  Munki::Application::MUNKITOOLS_VERSION = `/usr/local/munki/makepkginfo -V`
 else
   Munki::Application::MUNKITOOLS_VERSION = "not installed"
 end
