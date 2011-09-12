454231e5f69d880a835a8c50259ff0837c1611a1
Jordan Raine <jnraine@gmail.com>
2011-09-11 18:03:42 -0700

    Ensure user group cannot be a member of itself (UI and model constraint)

--- app/models/user_group_membership.rb
+++ app/models/user_group_membership.rb
@@ -1,7 +1,7 @@
 class UserGroupMembership < ActiveRecord::Base
   validates :principal_id, :presence => true
   validates :principal_type, :presence => true
-  validates :user_group_id, :presence => true
+  validates :user_group_id, :presence => true, :does_not_reference_self => true
   
   belongs_to :principal, :polymorphic => :true
   belongs_to :user_group
