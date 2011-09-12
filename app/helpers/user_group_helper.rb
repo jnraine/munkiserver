fa6f806a2e6738b99f535f69c5731ff9b564343e
Jordan Raine <jnraine@gmail.com>
2011-09-11 17:53:08 -0700

    Refactor user group membership using a virtual attribute, improve UI

--- app/helpers/user_group_helper.rb
+++ app/helpers/user_group_helper.rb
@@ -1,2 +1,5 @@
 module UserGroupHelper
+  def principal_list_item(principal, opts={})
+    render :partial => 'principal_list_item', :locals => {:principal => principal, :disabled => opts[:disabled]}
+  end
 end
