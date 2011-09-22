78033d6ba1d110bb4924d93b584103786b5cee2c
Ricky Chilcott <ricky@rbsavvy.com>
2011-09-22 09:04:49 -0400

    Changed regex for hostnames -- more simple and catches all.

--- app/models/manifest/computer.rb
+++ app/models/manifest/computer.rb
@@ -16,7 +16,7 @@ class Computer < ActiveRecord::Base
   validates_presence_of :name, :hostname, :mac_address
   
   validates_format_of :hostname,
-                      :with => ^(([a-z]|[a-z][a-z0-9\-]*[a-z0-9])\.)*([a-z]|[a-z][a-z0-9\-]*[a-z0-9])$,
+                      :with => /^(([a-z]|[a-z][a-z0-9\-]*[a-z0-9])\.)*([a-z]|[a-z][a-z0-9\-]*[a-z0-9])$/,
                       :message => "must only contain alphanumeric and hyphens characters"
     
   validates_format_of :mac_address, :with => /^([0-9a-f]{2}(:|$)){6}$/ # mac_address attribute must look something like ff:12:ff:34:ff:56
