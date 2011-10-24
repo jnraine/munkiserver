7856b38ccd1c8d94bcce3e8c01417fa7a98615af
Jordan Raine <jnraine@gmail.com>
2011-10-24 16:14:25 -0700

    Typo fix

--- app/sweepers/environment_sweeper.rb
+++ app/sweepers/environment_sweeper.rb
@@ -15,10 +15,11 @@ class EnvironmentSweeper < ActionController::Caching::Sweeper
     
     def expire_catalog(environment)
       # Expire the catalogs for all environments in this unit
-      Unit.all.each { |unit|
-        Rails.logger.info "CACHE: Expiring the catalogs for #{catalog_cache_key_generator(:unit_id => unit.id, :environment_id => environment.id}"
+      Unit.all.each do |unit|
+        cache_key = catalog_cache_key_generator(:unit_id => unit.id, :environment_id => environment.id)
+        Rails.logger.info "CACHE: Expiring the catalogs for #{key}"
         Rails.cache.delete catalog_cache_key_generator(:unit_id => unit.id, :environment_id => environment.id)
-      }
+      end
     end
 
 end
