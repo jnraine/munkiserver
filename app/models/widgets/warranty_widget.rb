7b3f5e129393e94fb75a63cdeccbd9f50d0a6b7c
Ricky Chilcott <ricky@rbsavvy.com>
2011-10-25 19:25:24 -0400

    Adding an expire_after scope and updating warranty widget to not show . closed #69

--- app/models/widgets/warranty_widget.rb
+++ app/models/widgets/warranty_widget.rb
@@ -4,7 +4,7 @@ class WarrantyWidget < DashboardWidget
     options = default_options.merge(options)
     if @expiring.nil? or options[:force_query]
       # Find all the warranty that belong to all the units of the current user
-      @expiring = Warranty.belong_to_unit(user_units).expire_before(options[:expire_before])
+      @expiring = Warranty.belong_to_unit(user_units).expire_before(options[:expire_before]).expire_after(Date.today)
     end
     @expiring
   end
