772f122f92b72f3df28b197cc55f27b523b406dd
Trey Dockendorf <treydock@gmail.com>
2011-12-07 16:56:52 -0600

    - Allow either receipts or installs to be nil rather than allowing both to be nil

--- app/models/package.rb
+++ app/models/package.rb
@@ -39,11 +39,11 @@ class Package < ActiveRecord::Base
   # For now have to allow nil as these fields both receipts and installs aren't
   # required together.  This facilitates MySQL usage where
   # Text/Blobs can't have default values
-  validates :receipts_plist, :plist => true, :allow_nil => true
-  validates :installs_plist, :plist => true, :allow_nil => true
+  validates :receipts_plist, :plist => true, :if => "installs_plist.nil?"
+  validates :installs_plist, :plist => true, :if => "receipts_plist.nil?"
   validates :raw_tags_plist, :plist => true
-  validates :receipts, :array => true, :allow_nil => true
-  validates :installs, :array => true, :allow_nil => true
+  validates :receipts, :array => true, :if => "installs_plist.nil?"
+  validates :installs, :array => true, :if => "receipts_plist.nil?"
   validates :raw_tags, :hash => true
   validates :version, :uniqueness_in_unit => true  
   validates :force_install_after_date_string, :date_time => true, :allow_blank => true
