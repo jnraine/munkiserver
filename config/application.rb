3da9f7fe92777800dd391b595ba02e7f7e68c408
Trey Dockendorf <treydock@tamu.edu>
2012-03-12 10:11:19 -0500

    - Load paperclip images into asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,10 @@ module Munki
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-        
+
+    # Add additional asset pipeline paths
+    config.assets.paths << Rails.root.join("app", "assets", "paperclip")
+
     # Load server configuration YAML file
     settings = nil
     begin
