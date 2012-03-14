ff5f2ec3a1d31d28c319862bcced6a297d959b0f
Trey Dockendorf <treydock@tamu.edu>
2012-03-14 10:05:44 -0500

    - Update development to not serve static assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,4 +23,7 @@ Munki::Application.configure do
   
   #Expands the lines which load the assets
   config.assets.debug = true
+
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
 end
