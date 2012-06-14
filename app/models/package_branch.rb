582622d75d7c81b6d8c85239ad0e3cfc8a4b80d8
Jordan Raine <jnraine@gmail.com>
2012-06-13 21:19:48 -0400

    Add downcase to package branch name conformation method

--- app/models/package_branch.rb
+++ app/models/package_branch.rb
@@ -29,7 +29,7 @@ class PackageBranch < ActiveRecord::Base
   # Conforms a string to the package branch name constraints
   # => Replaces anything that are not alpheranumrical to underscores
   def self.conform_to_name_constraints(name)
-    name.gsub(/[^A-Za-z0-9_]+/,"_")
+    name.downcase.gsub(/[^A-Za-z0-9_]+/,"_")
   end
   
   # Check if there exists a pacakge branch display name that matches the 
