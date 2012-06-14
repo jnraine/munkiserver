75aeb3c3ce872ff4f70410787b19686e4f74b9bb
Jordan Raine <jnraine@gmail.com>
2012-06-13 21:57:05 -0400

    Rename backgrounder log

--- lib/backgrounder.rb
+++ lib/backgrounder.rb
@@ -1,8 +1,10 @@
 module Backgrounder
+  LOG_PATH = "#{Rails.root}/log/backgrounder.log"
+  
   def call_rake(task, options = {})
     options[:rails_env] ||= Rails.env
     args = options.map { |key, value| "#{key.to_s.upcase}='#{value}'" }
-    system "rake #{task} #{args.join(' ')} --trace >> #{Rails.root}/log/rake.log &"
+    system "rake #{task} #{args.join(' ')} --trace >> #{LOG_PATH} 2>&1 &"
   end
   
   extend self
