357c77c6ba57f62e3ed9d993f5ae94bc657c5b46
Ricky Chilcott <ricky@rbsavvy.com>
2013-06-08 14:13:42 -0400

    Display active_support deprecation errors to stderr

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,4 +30,6 @@ Munki::Application.configure do
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
+  
+  config.active_support.deprecation = :stderr
 end
