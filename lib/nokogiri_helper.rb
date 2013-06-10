59de0ec6aa292fa17c4140fa43e1227280635326
Jordan Raine <jnraine@gmail.com>
2013-06-10 13:11:29 -0700

    Handle HTTPS, fix failing test, handle 404s

--- lib/nokogiri_helper.rb
+++ lib/nokogiri_helper.rb
@@ -1,13 +1,21 @@
+require 'net/http'
+require 'net/https'
+
 module NokogiriHelper
   def page(page_url)
     response = Nokogiri::HTML(open(URI.escape(page_url)))
-  rescue SocketError
-    NullObject.new
+  rescue Errno::ENOENT, OpenURI::HTTPError => e
+    nil
   end
   
   # Fetches page and returns redirect URL in location header
   def redirect_url(url_string)
-    Net::HTTP.get_response(URI.parse(URI.escape(url_string))).header["Location"]
+    url = URI.parse(URI.escape(url_string))
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true if url.scheme == "https"
+    req = Net::HTTP::Get.new(url.path, {'User-Agent' => 'Ruby'})
+    response = https.request(req)
+    response.header["location"]
   end
 
   extend self
